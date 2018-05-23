2bbb2dcfe7a6f15fa864efbb1241b9eb50705381
Sergio Muñoz <sergiomunlop@gmail.com>
2018-05-23 16:12:22 +0200

    get events and actions

--- ewetasker/data/sparql/channels.py
+++ ewetasker/data/sparql/channels.py
@@ -10,35 +10,73 @@ import json
 def get_all_channels():
 
     #Define the SparQL store
-    endpoint = 'http://localhost:3030/ewetaskerdataset/query'
+    endpoint = 'http://localhost:3030/ewetasker/query'
     store = sparqlstore.SPARQLUpdateStore()
     store.open((endpoint, endpoint))
 
     # Get channels
-    rq = """
+    get_channels_query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
 
-        SELECT ?subject ?label ?comment ?logo
+        SELECT ?channel ?label ?comment ?logo
         WHERE {
-        ?subject rdfs:subClassOf ewe:Channel ;
+        ?channel rdfs:subClassOf ewe:Channel ;
                 rdfs:label ?label ;
                 rdfs:comment ?comment .
         
-        OPTIONAL {?subject foaf:logo ?logo }
+        OPTIONAL {?channel foaf:logo ?logo }
         }
     """
 
+    
     channels = { "channels" : []}
-    channels_result = store.query(rq)
-    for s, l, c, lo in channels_result:
+    channels_result = store.query(get_channels_query)
+    index = 0
+    for uri, label, comment, logo in channels_result:
         # TODO: get channel logo
-        channels["channels"].append({"@id" : s.n3(), "rdfs:label" : l.n3(), "rdfs:comment" : c.n3()})
-        # TODO: get events and actions
+        channels["channels"].append({"@id" : uri, "rdfs:label" : label.n3(), "rdfs:comment" : comment.n3(), "events" : [], "actions" : []})
+        
+        # TODO: get actions and channels parameters
+        
+        # get actions
+        get_actions_query = """
+            PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
+            SELECT ?action ?label ?comment
+            WHERE {
+            ?action rdfs:subclassOf ewe:Action ;
+                    rdfs:domain %s;
+                    rdfs:label ?label ;
+                    rdfs:comment ?comment .
+            }
+        """ % (uri.n3())
+        actions_result = store.query(get_actions_query)
+        for action_uri, action_label, action_comment in actions_result:
+            channels["channels"][index]["actions"].append({"@id" : action_uri, "rdfs:label" : action_label.n3(), "rdfs:comment" : action_comment.n3()})
+
+        # get events
+        get_events_query = """
+            PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+            SELECT ?action ?label ?comment
+            WHERE {
+            ?action rdfs:subclassOf ewe:Event ;
+                    rdfs:domain %s;
+                    rdfs:label ?label ;
+                    rdfs:comment ?comment .
+            }
+        """ % (uri.n3())
+        events_result = store.query(get_events_query)
+        for event_uri, event_label, event_comment in events_result:
+            channels["channels"][index]["events"].append({"@id" : event_uri, "rdfs:label" : event_label.n3(), "rdfs:comment" : event_comment.n3()})
+
+        index+=1
     #print(channels)
     #print(store.query(rq).serialize(format='json-ld', indent=4))
     return json.dumps(channels)
