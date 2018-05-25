0ffda3da47f9bee4703cdd12a7b3cf7ec9dba5e4
Sergio Muñoz <sergiomunlop@gmail.com>
2018-05-25 12:15:14 +0200

    get action and events parameters

--- README.md
+++ README.md
@@ -12,3 +12,92 @@ This repository contains EWE Tasker Server module, written in Golang.
 | /rules  | GET  | Get a list of rules available at the platform  |
 | /rules/user/{userID}  | GET  | Get a list of rules available at the platform for the specified user |
 | /rules/place/{placeID}  | GET  | Get a list of rules available at the platform associated to a certain place |
+
+## Get Channels
+
+```
+[
+   {
+      "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/SmartTv",
+      "rdfs:label":"\"Connected door-lock\"@en",
+      "rdfs:comment":"\"This channel represents a simplified smarttv with simple capabilities\"",
+      "events":[
+
+      ],
+      "actions":[
+         {
+            "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/SwitchOn",
+            "rdfs:label":"\"Switch on\"@en",
+            "rdfs:comment":"\"This action will switch on the TV.\"@en",
+            "input_parameters":[
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/LocalIP",
+                  "rdfs:label":"\"Local IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the local IP of the TV.\"@en"
+               },
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/PublicIP",
+                  "rdfs:label":"\"Public IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the public IP of the TV.\"@en"
+               }
+            ],
+            "output_parameters":[
+
+            ]
+         },
+         {
+            "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/SwitchOff",
+            "rdfs:label":"\"Switch off\"@en",
+            "rdfs:comment":"\"This action will switch off the TV.\"@en",
+            "input_parameters":[
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/LocalIP",
+                  "rdfs:label":"\"Local IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the local IP of the TV.\"@en"
+               },
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/PublicIP",
+                  "rdfs:label":"\"Public IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the public IP of the TV.\"@en"
+               }
+            ],
+            "output_parameters":[
+
+            ]
+         },
+         {
+            "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/DisplayImage",
+            "rdfs:label":"\"Display image\"@en",
+            "rdfs:comment":"\"This action will display an image on the screen.\"@en",
+            "input_parameters":[
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/LocalIP",
+                  "rdfs:label":"\"Local IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the local IP of the TV.\"@en"
+               },
+               {
+                  "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/PublicIP",
+                  "rdfs:label":"\"Public IP\"@en",
+                  "rdfs:comment":"\"This parameter represents the public IP of the TV.\"@en"
+               }
+            ],
+            "output_parameters":[
+
+            ]
+         }
+      ],
+      "parameters":[
+         {
+            "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/LocalIP",
+            "rdfs:label":"\"Local IP\"@en",
+            "rdfs:comment":"\"This parameter represents the local IP of the TV.\"@en"
+         },
+         {
+            "@id":"http://gsi.dit.upm.es/ontologies/ewe-connected-home-smarttv/ns/PublicIP",
+            "rdfs:label":"\"Public IP\"@en",
+            "rdfs:comment":"\"This parameter represents the public IP of the TV.\"@en"
+         }
+      ]
+   }
+]
+```
--- ewetasker/data/sparql/channels.py
+++ ewetasker/data/sparql/channels.py
@@ -33,15 +33,13 @@ def get_all_channels():
     """
 
     
-    channels = { "channels" : []}
+    channels = []
     channels_result = store.query(get_channels_query)
     index = 0
     for uri, label, comment, logo in channels_result:
         # TODO: get channel logo
-        channels["channels"].append({"@id" : uri, "rdfs:label" : label.n3(), "rdfs:comment" : comment.n3(), "events" : [], "actions" : [], "parameters" : []})
-        
-        # TODO: get actions and channels parameters
-        
+        channels.append({"@id" : uri, "rdfs:label" : label.n3(), "rdfs:comment" : comment.n3(), "events" : [], "actions" : [], "parameters" : []})
+                
         # get actions
         get_actions_query = """
             PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
@@ -49,32 +47,118 @@ def get_all_channels():
 
             SELECT ?action ?label ?comment
             WHERE {
-            ?action rdfs:subclassOf ewe:Action ;
+            %s ewe:providesAction ?action .
+            ?action rdfs:subClassOf ewe:Action ;
                     rdfs:domain %s;
                     rdfs:label ?label ;
                     rdfs:comment ?comment .
             }
-        """ % (uri.n3())
+        """ % (uri.n3(), uri.n3())
         actions_result = store.query(get_actions_query)
-        for action_uri, action_label, action_comment in actions_result:
-            channels["channels"][index]["actions"].append({"@id" : action_uri, "rdfs:label" : action_label.n3(), "rdfs:comment" : action_comment.n3()})
+        action_index = 0
 
+        for action_uri, action_label, action_comment in actions_result:
+
+            channels[index]["actions"].append({"@id" : action_uri, "rdfs:label" : action_label.n3(), "rdfs:comment" : action_comment.n3(), "input_parameters" : [], "output_parameters" : []})
+
+            # get input parameters
+            get_input_parameters_query = """
+                PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?parameter ?label ?comment
+                WHERE {
+                %s ewe:hasInputParameter ?parameter .
+                ?parameter rdfs:subClassOf ewe:Parameter ;
+                        rdfs:domain %s;
+                        rdfs:label ?label ;
+                        rdfs:comment ?comment .
+                }
+            """ % (action_uri.n3(), uri.n3())
+
+            input_params_result = store.query(get_input_parameters_query)
+            for input_param_uri, input_param_label, input_param_comment in input_params_result:
+                channels[index]["actions"][action_index]["input_parameters"].append({"@id" : input_param_uri, "rdfs:label" : input_param_label.n3(), "rdfs:comment" : input_param_comment.n3()})
+
+            # get output parameters
+            get_output_parameters_query = """
+                PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?parameter ?label ?comment
+                WHERE {
+                %s ewe:hasOutputParameter ?parameter .
+                ?parameter rdfs:subClassOf ewe:Parameter ;
+                        rdfs:domain %s;
+                        rdfs:label ?label ;
+                        rdfs:comment ?comment .
+                }
+            """ % (action_uri.n3(), uri.n3())
+
+            output_params_result = store.query(get_output_parameters_query)
+            for output_param_uri, output_param_label, output_param_comment in output_params_result:
+                channels[index]["actions"][action_index]["output_parameters"].append({"@id" : output_param_uri, "rdfs:label" : output_param_label.n3(), "rdfs:comment" : output_param_comment.n3()})
+
+            action_index+=1
         # get events
         get_events_query = """
             PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-            SELECT ?action ?label ?comment
+            SELECT ?event ?label ?comment
             WHERE {
-            ?action rdfs:subclassOf ewe:Event ;
+            %s ewe:generatesEvent ?event .
+            ?event rdfs:subClassOf ewe:Event ;
                     rdfs:domain %s;
                     rdfs:label ?label ;
                     rdfs:comment ?comment .
             }
-        """ % (uri.n3())
+        """ % (uri.n3(), uri.n3())
+
+        event_index = 0
         events_result = store.query(get_events_query)
         for event_uri, event_label, event_comment in events_result:
-            channels["channels"][index]["events"].append({"@id" : event_uri, "rdfs:label" : event_label.n3(), "rdfs:comment" : event_comment.n3()})
+            channels[index]["events"].append({"@id" : event_uri, "rdfs:label" : event_label.n3(), "rdfs:comment" : event_comment.n3(), "input_parameters" : [], "output_parameters" : []})
+            
+            # get input parameters
+            get_input_parameters_query = """
+                PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?parameter ?label ?comment
+                WHERE {
+                %s ewe:hasInputParameter ?parameter .
+                ?parameter rdfs:subClassOf ewe:Parameter ;
+                        rdfs:domain %s;
+                        rdfs:label ?label ;
+                        rdfs:comment ?comment .
+                }
+            """ % (event_uri.n3(), uri.n3())
+
+            input_params_result = store.query(get_input_parameters_query)
+            for input_param_uri, input_param_label, input_param_comment in input_params_result:
+                channels[index]["events"][event_index]["input_parameters"].append({"@id" : input_param_uri, "rdfs:label" : input_param_label.n3(), "rdfs:comment" : input_param_comment.n3()})
+
+            # get output parameters
+            get_output_parameters_query = """
+                PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+                SELECT ?parameter ?label ?comment
+                WHERE {
+                %s ewe:hasOutputParameter ?parameter .
+                ?parameter rdfs:subClassOf ewe:Parameter ;
+                        rdfs:domain %s;
+                        rdfs:label ?label ;
+                        rdfs:comment ?comment .
+                }
+            """ % (event_uri.n3(), uri.n3())
+
+            output_params_result = store.query(get_output_parameters_query)
+            for output_param_uri, output_param_label, output_param_comment in output_params_result:
+                channels[index]["events"][event_index]["output_parameters"].append({"@id" : output_param_uri, "rdfs:label" : output_param_label.n3(), "rdfs:comment" : output_param_comment.n3()})
+
+            event_index+=1
 
         get_parameters_query = """
             PREFIX ewe: <http://gsi.dit.upm.es/ontologies/ewe/ns/>
@@ -90,7 +174,7 @@ def get_all_channels():
         """ % (uri.n3())
         parameters_result = store.query(get_parameters_query)
         for param_uri, param_label, param_comment in parameters_result:
-            channels["channels"][index]["parameters"].append({"@id" : param_uri, "rdfs:label" : param_label.n3(), "rdfs:comment" : param_comment.n3()})
+            channels[index]["parameters"].append({"@id" : param_uri, "rdfs:label" : param_label.n3(), "rdfs:comment" : param_comment.n3()})
 
         index+=1
     #print(channels)
