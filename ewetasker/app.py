1022fe90b2ee303e9096dc29ee10e83439a31c37
Sergio Muñoz <sergiomunlop@gmail.com>
2018-06-03 18:26:52 +0200

    handle json

--- ewetasker/app.py
+++ ewetasker/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from delivery.channels import *
 from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,10 @@ CORS(app)
 def channels():
     return get_channels()
 
+@app.route("/channels/import", methods = ['POST'])
+def import_channel():
+    re = request.get_json()
+    return re["@id"]
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
